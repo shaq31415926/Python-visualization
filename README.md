de5d41cc6e334ac2a11a9cc0b0af5263450f149f
shaq31415926 <sarah.haq@upday.com>
2020-12-11 23:19:15 +0100

    included correlation plots + missing data visualization

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Python-visualization
+### Exploring Data Visualization for Machine Learning Projects
+by Sarah Haq
+
+#### Content
+Beginner to intermediate users are the target audience. Examples of EDA and visualizations in Python for Machine Learning models.
+
+#### Requirements
+Jupyter Notebooks with Python 3.7 +
+
+
+#### Structure
+notebooks/01-sh-data-prep.ipynb
+notebooks/02-sh-eda.ipynb
---
+++ notebooks/identify_missing.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_missing(missing_stats):
+        """Histogram of missing value for every features"""
+        plt.style.use('seaborn-white')
+        plt.figure(figsize=(7, 5))
+        plt.hist(missing_stats['missing_fraction'], bins=np.linspace(0, 1, 11), edgecolor='k', color='red',
+                 linewidth=1.5)
+        plt.xticks(np.linspace(0, 1, 11))
+        plt.xlabel('Missing Fraction', size=14)
+        plt.ylabel('Count of Features', size=14)
+        plt.title("Fraction of Missing Values Histogram", size=16)
+
+        plt.show()
+        
+def identify_missing(data, missing_threshold):
+    """Find the features with a fraction of missing values above `missing_threshold`"""
+    missing_series = data.isnull().sum() / data.shape[0]
+    missing_stats = pd.DataFrame(
+            missing_series).rename(columns={'index': 'feature',
+                                                 0 : 'missing_fraction'})
+
+    # Sort with highest number of missing values on top
+    missing_stats = missing_stats.sort_values('missing_fraction', ascending=False)
+
+    # Find the columns with a missing percentage above the threshold
+    record_missing = pd.DataFrame(
+        missing_series[missing_series > missing_threshold]).reset_index().rename(
+        columns={'index': 'feature',
+                 0: 'missing_fraction'})
+    
+    plot_missing(missing_stats)
+
+    to_drop = list(record_missing['feature'])
+
+    print('%d features with greater than %0.2f missing values.\n' % (
+            len(to_drop), missing_threshold))
